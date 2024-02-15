4fd6d36e6a9ec75f89e7ad2ae11c13fe0f1457b6
regmi2 <regmi2@wisc.edu>
2024-02-15 16:17:52 -0600

    navbar, update prisma schema, sign in, user, sign out options on the navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,12 @@ import { api } from "~/utils/api";
 import {ClerkProvider} from '@clerk/nextjs'
 
 import "~/styles/globals.css";
+import { Navbar } from "~/components/Navbar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
       return (
-      <ClerkProvider {...pageProps}>
+        <ClerkProvider {...pageProps}>
+          <Navbar/>
         <Component {...pageProps} />
       </ClerkProvider>
     );
---
+++ src/pages/contact.tsx
@@ -0,0 +1,17 @@
+export default function Contact() {
+    return (
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+            <p className="text-2xl">
+                Contact
+            </p>
+            <p className="text-2xl ">
+               email@email.com
+            </p>
+            <p className="text-2xl">
+                +1 202-555-0101
+            </p>
+        </div>
+
+            
+)
+}
